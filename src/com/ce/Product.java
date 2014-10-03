0cdb90e65e24ea20d0bd24b531dc4d1d07aa1b0d
larrysoup <a99402351@gmail.com>
2014-10-03 15:32:02 +0800

    Update Person about variable.  Test Person.

--- src/com/ce/Product.java
+++ src/com/ce/Product.java
@@ -2,31 +2,22 @@ package com.ce;
 
 
 public class Product{
-	private int id;
-	private final static int PI = 10;
 	
-	
-
-	public int getId() {
-		return id;
-	}
-
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-
 	public static void main(String[] args) {
-		Product pt = new Product();
+		Person[] people = {
+			new Person("Mark", 18, 180.5, 78, "男"), 
+			new Person("Lily", 20, 168.4, 45, "女"),
+			new Person("Bob", 45, 170.2, 80, "Man")
+		};
 		
-		pt.setId(50);
-		System.out.println(pt.getId());
-		pt.setId( pt.getId() + PI );
-		System.out.println(pt.getId());
-		
+		for (Person p : people) {
+			System.out.println("姓名: " + p.getName());
+			System.out.println("年齡: " + p.getAge());
+			System.out.println("身高: " + p.getHight());
+			System.out.println("體重: " + p.getWeight());
+			System.out.println("性別: " + p.getGender());
+			System.out.println("===========================");
+		}
 	}
 
 
